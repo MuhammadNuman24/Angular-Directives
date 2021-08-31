42ce5444511f851d95b899d09f4fcc9ded647ecb
Muhammad Numan <numanrazzaq24@gmail.com>
2021-08-31 10:13:46 +0500

    NgFor directive

---
+++ src/app/app.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'app';
+  header= "Hello World";
+  colors:string[] =[
+    'red' , 'green' , 'blue' , "white",'orange'
+  ]
+
+newcolor :string= "";
+addcolor(){
+  this.colors.push(this.newcolor)
+}
+
+public date :Date = new Date();
+public rupee :number = 30.45;
+
+
+}
